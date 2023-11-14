dc39a37f55d5765fc86f816f1c88a531e6ff39a4
karimalydev <147738989+karimalydev@users.noreply.github.com>
2023-11-14 12:58:25 +0000

    Front end complete
    created instructions section (timeline) 
    and completed the rest of the front end

---
+++ src/components/InstructionsGame.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-timeline>
+
+
+    <v-timeline-item dot-color="purple-lighten-2" fill-dot>
+      <v-card>
+        <v-card-title class="bg-purple-lighten-2">
+          <v-icon size="large" class="me-4">fas fa-1</v-icon>
+        </v-card-title>
+        <v-card-text>Have a Look at all the Puzzle pieces Provided below</v-card-text>
+      </v-card>
+    </v-timeline-item>
+
+    
+    <v-timeline-item dot-color="amber-lighten-1" fill-dot size="x-small">
+      <v-card>
+        <v-card-title class="bg-amber-lighten-1 ">
+            <v-icon size="large">fas fa-2</v-icon>
+        </v-card-title>
+        <v-card-text> Procceed to click on the grid to draw a piece and submit it! You can also Undo by clicking on the cell again.</v-card-text>
+      </v-card>
+    </v-timeline-item>
+
+    
+    <v-timeline-item dot-color="cyan-lighten-1" fill-dot>
+      <v-card>
+        <v-card-title class="bg-cyan-lighten-1">
+          <v-icon  size="large">fas fa-3</v-icon>
+        </v-card-title>
+        <v-card-text>You can Test Yourself by trying to fill the entire grid and we'll let you know if you are on track</v-card-text>
+      </v-card>
+    </v-timeline-item>
+
+    
+    <v-timeline-item dot-color="red-lighten-1" fill-dot size="x-small" >
+      <v-card>
+        <v-card-title class="bg-red-lighten-1 justify-end">
+            <v-icon size="large" >fas fa-4</v-icon>
+        </v-card-title>
+        <v-card-text>After Submitting a Piece you can press "Generate Solution" to see how the grid can fil up with your initial configuration </v-card-text>
+      </v-card>
+    </v-timeline-item>
+
+    
+    <v-timeline-item dot-color="green-lighten-1" fill-dot>
+      <v-card>
+        <v-card-title class="bg-green-lighten-1">
+          <v-icon class="me-4" size="large" >fas fa-5</v-icon>
+        </v-card-title>
+        <v-card-text> ENJOY & PLAY AGAIN </v-card-text>
+      </v-card>
+    </v-timeline-item>
+
+  </v-timeline>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@
         </v-col>
         <v-col cols="5">
           <div style="position:relative;z-index:9999;" class="mt-16">
-            <v-img src="i1.png" contain max-height="300"></v-img>
+            <v-img src="i1.png" contain max-height="290"></v-img>
           </div>
         </v-col>
       </v-row>  
@@ -27,18 +27,32 @@
   </v-container>
 
 
+<v-container fluid id="instructions">
+   
+<div class="instructions mt-16">
 
+<InstructionsGame />
+</div>
+  </v-container>
 
-  <v-container fluid>
+<v-container fluid id="play">
    
 <div class="game mt-16">
 
 <PuzzleComponent />
 </div>
   </v-container>
-  
-   
   </v-app>
+
+  <v-footer class="d-flex flex-column">
+    <div class="bg-teal d-flex w-100 align-center px-4">
+      <strong>Advanced Software Engineering Task 3 - Group 7</strong>
+      <v-spacer></v-spacer>
+    </div>
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Maohao Ran & Karim Aly</strong>
+    </div>
+  </v-footer>
 </template>
 
 <script>
@@ -48,13 +62,15 @@ import { defineComponent } from 'vue';
 
 import NavBar from '@/components/NavBar.vue';
 import PuzzleComponent from '@/components/PuzzleComponent.vue';
+import InstructionsGame from '@/components/InstructionsGame.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
     NavBar,
-    PuzzleComponent
+    PuzzleComponent,
+    InstructionsGame
   },
 });
 </script>
@@ -66,7 +82,7 @@ export default defineComponent({
   position: relative;
   text-align: center;
   padding: 12px;
-  margin-bottom: 6px;
+  margin-bottom: 8px;
   height: 400px;
   width: 100%;
   color: white;
@@ -79,7 +95,7 @@ export default defineComponent({
   height: 100%;
   width: 50%;
   background: pink ;
-  transform: skew(0deg, 6deg);
+  transform: skew(0deg, 7deg);
 }
 .head::after{
   content: "";
@@ -89,7 +105,7 @@ export default defineComponent({
   height: 100%;
   width: 50%;
   background: pink ;
-  transform: skew(0deg, -6deg);
+  transform: skew(0deg, -7deg);
 
 }
 </style>
